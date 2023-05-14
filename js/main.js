d263f85609637debca214e43b4585b1bccfa0da6
Marco Antônio da Silva <world.solucoes.eletrica@hotmail.com>
2023-05-14 14:46:42 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// MDB Lightbox Init
+$(function () {
+    $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+    });
+
+
+(function($) {
+
+    "use strict";
+    /** Inicializar nosso contador**/
+    $('.contar').counterUp({
+        time: 1000,
+        delay: 10
+    });
+})(jQuery);
+
+
